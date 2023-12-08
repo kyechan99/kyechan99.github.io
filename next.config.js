bed8541657ffa80dee4409eaa240da4274bc3cee
kyechan99 <kyechan99@gmail.com>
2023-12-08 19:39:50 +0900

    export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const { withContentlayer } = require("next-contentlayer");
 
-const isProduction = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: isProduction ? "export" : undefined,
+  output: "export",
   images: {
     unoptimized: true,
   },
